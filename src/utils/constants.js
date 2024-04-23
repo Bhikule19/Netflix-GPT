e49a61b0e1c036b19afed9f2095f39c8dd991ba7
Abhishek <abhishekbhikule76@gmail.com>
2024-04-23 11:16:17 +0530

    Netlify

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 export const API_CALL = {
   method: "GET",
   headers: {
-    Authorization: "Bearer" + process.env.REACT_APP_TMDB_KEY,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTI5ZDY2OWZmM2UzZjAzNmU1NWJjYjhkNDA0ODUxNCIsInN1YiI6IjYzYWJlZmUyN2VmMzgxMWY2MDg0ZWFhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FUAaNSS02wXORTPyyGeJBiPyqTxUYb2DsOlNgi25GF8",
     accept: "application/json",
   },
 };
